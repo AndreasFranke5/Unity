982c68090c3c74d991b16b4cdab7a0ec202cbd3c
Andreas Franke <afranke@ug.kth.se>
2024-10-02 14:46:13 +0200

    7

--- CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -11,8 +11,8 @@ public class PlayerController : MonoBehaviour
         
     }
     public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10;
+    public float speed = 20.0f;
+    public float xRange = 20;
     public GameObject projectilePrefab;
     public Transform firePoint;
     // Update is called once per frame
